c75423609ca7f5bca9e35aa44add55e62434b2b2
Robert A. McDougal <robert.mcdougal@yale.edu>
2019-09-24 08:53:41 -0400

    initial model

---
+++ demo/demo/model.py
@@ -0,0 +1,23 @@
+# usually you would pull this from a database but we'll hardcode some data
+cell_data = {
+    "1": {
+        "name": "CA1 pyramidal neuron",
+        "location": "brain",
+        "role": "learning and memory"
+    },
+    "2": {
+        "name": "Cardiac purkinje cell",
+        "location": "heart",
+        "role": "propagate contraction signals"
+    },
+    "3": {
+        "name": "Pancreatic beta cell",
+        "location": "pancreas",
+        "role": "synthesize and secrete insulin"
+}
+
+class Model:
+    def get_cells():
+        return [id_ for id_ in cell_data]
+    def get_cell(id_):
+        return cell_data.get(id_)
